175b7c51dbf9e06619a4cc3f612e8a273535fc40
Nekromancer <jakub.nekro@gmail.com>
2018-10-12 13:58:01 +0200

    Reuse transaction in BaseTransaction (not working yet, only api update)

--- src/SoundFingerprinting.Extensions.LMDB/LMDBDatabase/BaseTransaction.cs
+++ src/SoundFingerprinting.Extensions.LMDB/LMDBDatabase/BaseTransaction.cs
@@ -18,123 +18,63 @@ namespace SoundFingerprinting.Extensions.LMDB.LMDBDatabase
             this.indexesHolder = indexesHolder;
         }
 
-        protected TrackDataDTO GetTrackById(ref DirectBuffer id, object transaction)
+        protected TrackDataDTO GetTrackById(ref DirectBuffer id, Spreads.LMDB.ReadOnlyTransaction transaction)
         {
-            if (transaction is Transaction tx)
+            if (databasesHolder.TracksDatabase.TryGet(transaction, ref id, out DirectBuffer value))
             {
-                if (databasesHolder.TracksDatabase.TryGet(tx, ref id, out DirectBuffer value))
-                {
-                    return ZeroFormatterSerializer.Deserialize<TrackDataDTO>(value.Span.ToArray());
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            else if (transaction is Spreads.LMDB.ReadOnlyTransaction rotx)
-            {
-                if (databasesHolder.TracksDatabase.TryGet(rotx, ref id, out DirectBuffer value))
-                {
-                    return ZeroFormatterSerializer.Deserialize<TrackDataDTO>(value.Span.ToArray());
-                }
-                else
-                {
-                    return null;
-                }
+                return ZeroFormatterSerializer.Deserialize<TrackDataDTO>(value.Span.ToArray());
             }
             else
             {
-                throw new ArgumentException("Not an Transaction object", nameof(transaction));
+                return null;
             }
         }
 
-        protected SubFingerprintDataDTO GetSubFingerprint(ulong id, object transaction)
+        protected SubFingerprintDataDTO GetSubFingerprint(ulong id, Spreads.LMDB.ReadOnlyTransaction transaction)
         {
             var subFingerprintKey = BitConverter.GetBytes(id).AsMemory();
             var value = default(DirectBuffer);
-            var result = false;
             using (subFingerprintKey.Pin())
             {
                 var keyBuffer = new DirectBuffer(subFingerprintKey.Span);
-                if (transaction is Transaction tx)
+                if (databasesHolder.SubFingerprintsDatabase.TryGet(transaction, ref keyBuffer, out value))
                 {
-                    result = databasesHolder.SubFingerprintsDatabase.TryGet(tx, ref keyBuffer, out value);
-                }
-                else if (transaction is Spreads.LMDB.ReadOnlyTransaction rotx)
-                {
-                    result = databasesHolder.SubFingerprintsDatabase.TryGet(rotx, ref keyBuffer, out value);
+                    return ZeroFormatterSerializer.Deserialize<SubFingerprintDataDTO>(value.Span.ToArray());
                 }
                 else
                 {
-                    throw new ArgumentException("Not an Transaction object", nameof(transaction));
+                    throw new KeyNotFoundException();
                 }
-            }
-
-            if (result)
-            {
-                return ZeroFormatterSerializer.Deserialize<SubFingerprintDataDTO>(value.Span.ToArray());
-            }
-            else
-            {
-                throw new KeyNotFoundException();
             }
         }
 
-        protected List<SubFingerprintDataDTO> GetSubFingerprintsForTrack(ulong id, object transaction)
+        protected List<SubFingerprintDataDTO> GetSubFingerprintsForTrack(ulong id, Spreads.LMDB.ReadOnlyTransaction transaction)
         {
             var trackKey = BitConverter.GetBytes(id).AsMemory();
             var list = new List<SubFingerprintDataDTO>();
 
-            if (transaction is Transaction tx)
+            using (trackKey.Pin())
+            using (var cursor = indexesHolder.TracksSubfingerprintsIndex.OpenReadOnlyCursor(transaction))
             {
-                using (trackKey.Pin())
-                using (var cursor = indexesHolder.TracksSubfingerprintsIndex.OpenCursor(tx))
+                var keyBuffer = new DirectBuffer(trackKey.Span);
+                var valueBuffer = default(DirectBuffer);
+                if (cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.Set)
+                    && cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.FirstDuplicate))
                 {
-                    var keyBuffer = new DirectBuffer(trackKey.Span);
-                    var valueBuffer = default(DirectBuffer);
-                    if (cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.Set)
-                        && cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.FirstDuplicate))
-                    {
-                        var subFingerprintId = valueBuffer.ReadUInt64(0);
-                        list.Add(GetSubFingerprint(subFingerprintId, tx));
+                    var subFingerprintId = valueBuffer.ReadUInt64(0);
+                    list.Add(GetSubFingerprint(subFingerprintId, transaction));
 
-                        while (cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.NextDuplicate))
-                        {
-                            subFingerprintId = valueBuffer.ReadUInt64(0);
-                            list.Add(GetSubFingerprint(subFingerprintId, tx));
-                        }
+                    while (cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.NextDuplicate))
+                    {
+                        subFingerprintId = valueBuffer.ReadUInt64(0);
+                        list.Add(GetSubFingerprint(subFingerprintId, transaction));
                     }
                 }
-            }
-            else if (transaction is Spreads.LMDB.ReadOnlyTransaction rotx)
-            {
-                using (trackKey.Pin())
-                using (var cursor = indexesHolder.TracksSubfingerprintsIndex.OpenReadOnlyCursor(rotx))
-                {
-                    var keyBuffer = new DirectBuffer(trackKey.Span);
-                    var valueBuffer = default(DirectBuffer);
-                    if (cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.Set)
-                        && cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.FirstDuplicate))
-                    {
-                        var subFingerprintId = valueBuffer.ReadUInt64(0);
-                        list.Add(GetSubFingerprint(subFingerprintId, rotx));
-
-                        while (cursor.TryGet(ref keyBuffer, ref valueBuffer, CursorGetOption.NextDuplicate))
-                        {
-                            subFingerprintId = valueBuffer.ReadUInt64(0);
-                            list.Add(GetSubFingerprint(subFingerprintId, rotx));
-                        }
-                    }
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Not an Transaction object", nameof(transaction));
             }
             return list;
         }
 
-        protected Span<ulong> GetSubFingerprintsByHashTableAndHash(int table, int hash, object transaction)
+        protected Span<ulong> GetSubFingerprintsByHashTableAndHash(int table, int hash, Spreads.LMDB.ReadOnlyTransaction transaction)
         {
             var tableDatabase = databasesHolder.HashTables[table];
             var key = hash;
@@ -142,57 +82,26 @@ namespace SoundFingerprinting.Extensions.LMDB.LMDBDatabase
 
             ulong[] buffer = null;
 
-            if (transaction is Transaction tx)
+            using (var cursor = tableDatabase.OpenReadOnlyCursor(transaction))
             {
-                using (var cursor = tableDatabase.OpenCursor(tx))
+                if (cursor.TryGet(ref key, ref value, CursorGetOption.Set)
+                    && cursor.TryGet(ref key, ref value, CursorGetOption.FirstDuplicate))
                 {
-                    if (cursor.TryGet(ref key, ref value, CursorGetOption.Set)
-                        && cursor.TryGet(ref key, ref value, CursorGetOption.FirstDuplicate))
+                    var counter = 0;
+                    buffer = new ulong[cursor.Count()];
+                    buffer[counter] = value;
+
+                    while (cursor.TryGet(ref key, ref value, CursorGetOption.NextDuplicate))
                     {
-                        var counter = 0;
-                        buffer = new ulong[cursor.Count()];
+                        counter++;
                         buffer[counter] = value;
+                    }
 
-                        while (cursor.TryGet(ref key, ref value, CursorGetOption.NextDuplicate))
-                        {
-                            counter++;
-                            buffer[counter] = value;
-                        }
-
-                        if (counter != (buffer.Length - 1))
-                        {
-                            throw new Exception("Bad buffer length");
-                        }
+                    if (counter != (buffer.Length - 1))
+                    {
+                        throw new Exception("Bad buffer length");
                     }
                 }
-            }
-            else if (transaction is Spreads.LMDB.ReadOnlyTransaction rotx)
-            {
-                using (var cursor = tableDatabase.OpenReadOnlyCursor(rotx))
-                {
-                    if (cursor.TryGet(ref key, ref value, CursorGetOption.Set)
-                        && cursor.TryGet(ref key, ref value, CursorGetOption.FirstDuplicate))
-                    {
-                        var counter = 0;
-                        buffer = new ulong[cursor.Count()];
-                        buffer[counter] = value;
-
-                        while (cursor.TryGet(ref key, ref value, CursorGetOption.NextDuplicate))
-                        {
-                            counter++;
-                            buffer[counter] = value;
-                        }
-
-                        if (counter != (buffer.Length - 1))
-                        {
-                            throw new Exception("Bad buffer length");
-                        }
-                    }
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Not an Transaction object", nameof(transaction));
             }
 
 #pragma warning disable RCS1084 // Use coalesce expression instead of conditional expression.
